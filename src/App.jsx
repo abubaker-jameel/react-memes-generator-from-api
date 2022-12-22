ffbb0baf398ee131d1ef85057c1ee2c3eeab8f22
abubaker <codecookies958@gmail.com>
2022-12-22 16:03:04 +0500

    feat: Added functionality to generate new meme and set the top and bottom texts

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import Form from "./Form";
 import Header from "./Header";
 import MemeImage from "./MemeImage";
+import memeData from "./memesData";
 
 export default function App() {
+  const [formData, setFormData] = useState({
+    topText: "",
+    bottomText: "",
+  });
+  console.log(formData);
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [name]: value,
+    }));
+  }
+  const [memeImage, setMemeImage] = useState(memeData.data.memes[10].url);
+  function handleImageGenerate() {
+    const randomMemeLength = Math.floor(
+      Math.random() * memeData.data.memes.length
+    );
+    const newMeme = memeData.data.memes[randomMemeLength];
+    const newUrl = newMeme.url;
+    setMemeImage(newUrl);
+  }
   return (
     <>
       <div className="container">
         <Header />
-        <Form />
-        <MemeImage />
+        <Form
+          topValue={formData.topText}
+          bottomValue={formData.bottomText}
+          handleChangeEvent={handleChange}
+          handleNewImage={handleImageGenerate}
+        />
+        <MemeImage
+          topTextValue={formData.topText}
+          bottomTextValue={formData.bottomText}
+          imageUrl={memeImage}
+        />
       </div>
     </>
   );
--- src/Form.jsx
+++ src/Form.jsx
@@ -1,12 +1,24 @@
 import React from "react";
 
-export default function Form() {
+export default function Form(props) {
   return (
     <>
       <div className="form">
-        <input type="text" name="" id="" placeholder="Top Text" />
-        <input type="text" name="" id="" placeholder="Bottom Text" />
-        <button>Get a new meme image 🖼</button>
+        <input
+          type="text"
+          name="topText"
+          placeholder="Top Text"
+          onChange={props.handleChangeEvent}
+          value={props.topValue}
+        />
+        <input
+          type="text"
+          name="bottomText"
+          placeholder="Bottom Text"
+          onChange={props.handleChangeEvent}
+          value={FormData.bottomValue}
+        />
+        <button onClick={props.handleNewImage}>Get a new meme image 🖼</button>
       </div>
     </>
   );
